ad5865c647c31d82064d33b329ee72476d215b5a
jinkirino <jinkirino@gmail.com>
2022-05-23 23:30:06 +0900

    First Commit

---
+++ AnzanApp/AnzanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnzanAppApp.swift
+//  AnzanApp
+//
+//  Created by 神　樹里乃 on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AnzanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnzanApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnzanApp
+//
+//  Created by 神　樹里乃 on 2022/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("暗算アプリ")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
