2e2a46fb49675c8eaf50cc7f14139f60c37bd1ab
jinkirino <jinkirino@gmail.com>
2022-05-24 12:40:46 +0900

    答え合わせのsheetを作成

---
+++ AnzanApp/AnswersView.swift
@@ -0,0 +1,20 @@
+//
+//  AnswersView.swift
+//  AnzanApp
+//
+//  Created by 神　樹里乃 on 2022/05/24.
+//
+
+import SwiftUI
+
+struct AnswersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnswersView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswersView()
+    }
+}
